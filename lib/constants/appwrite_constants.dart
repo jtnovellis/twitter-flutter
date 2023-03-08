9ab1afd4ddb4494723073c21e5a0ec51d0512d85
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2023-03-07 21:59:57 -0500

    initial commit

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '6407eaf29ae777ed4572';
+  static const String projectId = '6407e7b0b17f4166bb19';
+  static const String endpoint = 'http://localhost:80/v1';
+}
