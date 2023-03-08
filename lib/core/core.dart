878dff63e6df7a45711e18c6f6c47d00ac918cca
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2023-03-08 01:53:01 -0500

    feat: add signup funcionality with appwrite

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,3 +1,4 @@
 export './failure.dart';
 export './types_def.dart';
 export './providers.dart';
+export './utils.dart';
--- lib/features/view/signup_view.dart
+++ lib/features/view/signup_view.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/common/common.dart';
 import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/features/controller/auth_controller.dart';
 import 'package:twitter_clone/features/view/view.dart';
 import 'package:twitter_clone/features/widgets/widgets.dart';
 import 'package:twitter_clone/theme/theme.dart';
 import 'package:flutter/gestures.dart';
 
-class SignupView extends StatefulWidget {
+class SignupView extends ConsumerStatefulWidget {
   static route() => MaterialPageRoute(
         builder: (context) => const SignupView(),
       );
@@ -14,10 +16,10 @@ class SignupView extends StatefulWidget {
   const SignupView({super.key});
 
   @override
-  State<SignupView> createState() => _SignupViewState();
+  ConsumerState<SignupView> createState() => _SignupViewState();
 }
 
-class _SignupViewState extends State<SignupView> {
+class _SignupViewState extends ConsumerState<SignupView> {
   final appbar = UIConstants.appBar();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
@@ -29,73 +31,85 @@ class _SignupViewState extends State<SignupView> {
     _passwordController.dispose();
   }
 
+  void onSignUp() {
+    ref.read(authControllerProvider.notifier).signUp(
+          email: _emailController.text,
+          password: _passwordController.text,
+          context: context,
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(authControllerProvider);
+
     return Scaffold(
       appBar: appbar,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-            ),
-            child: Column(
-              children: [
-                AuthField(
-                  controller: _emailController,
-                  hintText: 'Enter the email',
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                AuthField(
-                  controller: _passwordController,
-                  hintText: 'Enter the password',
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: RoundedSmallButton(
-                    textColor: Pallete.backgroundColor,
-                    backgroundColor: Pallete.whiteColor,
-                    onTab: () {},
-                    label: 'Done',
+      body: isLoading
+          ? const Loader()
+          : Center(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
                   ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                RichText(
-                  text: TextSpan(
-                    text: 'Already have an account?',
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
+                  child: Column(
                     children: [
-                      TextSpan(
-                        text: ' Log In',
-                        style: const TextStyle(
-                          color: Pallete.blueColor,
-                          fontSize: 16,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            Navigator.push(
-                              context,
-                              LoginView.route(),
-                            );
-                          },
+                      AuthField(
+                        controller: _emailController,
+                        hintText: 'Enter the email',
                       ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      AuthField(
+                        controller: _passwordController,
+                        hintText: 'Enter the password',
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: RoundedSmallButton(
+                          textColor: Pallete.backgroundColor,
+                          backgroundColor: Pallete.whiteColor,
+                          onTab: onSignUp,
+                          label: 'Done',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          text: 'Already have an account?',
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: ' Log In',
+                              style: const TextStyle(
+                                color: Pallete.blueColor,
+                                fontSize: 16,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.push(
+                                    context,
+                                    LoginView.route(),
+                                  );
+                                },
+                            ),
+                          ],
+                        ),
+                      )
                     ],
                   ),
-                )
-              ],
+                ),
+              ),
             ),
-          ),
-        ),
-      ),
     );
   }
 }
